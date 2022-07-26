91507a729e5f4ed186782610fc02c9db74b3bbe4
Angel <mra7588@gmail.com>
2022-07-25 19:41:42 -0800

    component Navbar modificado

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styles from './Navbar.module.css';
-import { ActiveLink } from './ActiveLink';
+import { ActiveLink } from './ActiveLink'
 
 const menuItems = [
   {
